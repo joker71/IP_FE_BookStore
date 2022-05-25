04e9e72c1ff2eaf15d2ca0b4a6e125cc8171e2e9
DESKTOP-I4FPN0M\nhath <nhathoangn0@gmail.com>
2022-05-25 19:58:19 +0700

    rebase structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,59 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CheckoutComponent } from './pages/checkout/checkout.component';
-import { CartDetailsComponent } from './pages/cart-details/cart-details.component';
-import { BooklistComponent } from './pages/booklist/booklist.component';
-import { BookdetailComponent } from './pages/bookdetail/bookdetail.component';
-import { SearchComponent } from './pages/search/search.component';
-import { AsideComponent } from './pages/aside/aside.component';
-import { HeaderComponent } from './pages/header/header.component';
-import {RouterModule, Routes} from "@angular/router";
-import {HttpClientModule} from "@angular/common/http";
-import {ReactiveFormsModule} from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-const routers: Routes = [
-    {path: 'checkout', component: CheckoutComponent},
-    {path: 'cart-details', component: CartDetailsComponent},
-    {path: 'books/:id', component: BookdetailComponent},
-    {path: 'books', component: BooklistComponent},
-    {path: 'search/:keyword', component: BooklistComponent},
-    {path: 'category/:id', component: BooklistComponent},
-    {path: '', redirectTo: '/books', pathMatch: 'full'},
-    {path: '**', redirectTo: '/books', pathMatch:'full'}
-]
+import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookService } from './services/book.service';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { BookCategoryComponent } from './components/book-category/book-category.component';
+import { SearchComponent } from './components/search/search.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ReactiveFormsModule } from '@angular/forms';
+//client side paging
+//import { JwPaginationComponent } from 'jw-angular-pagination';
+
+
+const routes: Routes = [
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'cart-details', component: CartDetailsComponent},
+  {path: 'books/:id', component: BookDetailsComponent},
+  {path: 'books', component: BookListComponent},
+  {path: 'search/:keyword', component: BookListComponent},
+  {path: 'category/:id', component: BookListComponent},
+  {path: '', redirectTo: '/books', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
+
 @NgModule({
   declarations: [
-  
-    CheckoutComponent,
-       CartDetailsComponent,
-       BooklistComponent,
-       BookdetailComponent,
-       SearchComponent,
-       AsideComponent,
-       HeaderComponent,
+    AppComponent,
+    BookListComponent,
+    PageNotFoundComponent,
+    BookCategoryComponent,
+    SearchComponent,
+    BookDetailsComponent,
+    CartStatusComponent,
+    CartDetailsComponent,
+    CheckoutComponent
+    //client side paging
+    //JwPaginationComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-      ReactiveFormsModule,
-      RouterModule.forRoot(routers),
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: []
+  providers: [
+    BookService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
